67a606881413ca55eeee2fa14c07204976d435ce
Timothy Brantley II <tbrantleyii@gmail.com>
2021-01-28 16:03:52 -0500

    test: config is functioning better test

--- test/B_Config/A_Config_Test.php
+++ test/B_Config/A_Config_Test.php
@@ -1,5 +1,6 @@
 <?php
 /**
+ * Testing the config file and making sure all the spots are correctly filled
  */
 
 class A_Config_Test extends \Test\OpenTHC_Base_TestCase
@@ -9,26 +10,46 @@ class A_Config_Test extends \Test\OpenTHC_Base_TestCase
 
 	protected function setUp(): void 
 	{
-		$dir = APP_ROOT;
+		$dir = dirname(dirname(__dir__))."/etc";
 
 		$this->assertDirectoryExists($dir);
 		$this->assertDirectoryIsReadable($dir);
 
-		$ini_file_loc = sprintf('%s/etc/cre.ini', $dir);
+		$ini_file_loc = sprintf('%s/cre.ini', $dir);
 
 		$this->assertFileExists($ini_file_loc);
 		$this->assertFileIsReadable($ini_file_loc);
 
-		$ini_file_list[] = sprintf('%s/etc/cre.ini', $dir);
+		$this->cre_data = parse_ini_file($ini_file_loc, true, INI_SCANNER_RAW);
 
-		foreach ($ini_file_list as $ini_file) {
-			$cre_data = parse_ini_file($ini_file, true, INI_SCANNER_RAW);
-			break;
-		}
+		$this->assertNotNull($this->cre_data);
+		$this->assertNotEmpty($this->cre_data);
+		$this->assertIsArray($this->cre_data);
 	}
 
 	function test_config_lib()
 	{
-		// Test out Bundled cre.ini file
+	 	foreach ($this->cre_data as $cre) {
+			//if engine is metrc assert service key
+
+			$this->assertNotEmpty($cre);
+			$this->assertArrayHasKey('name', $cre, sprintf('%s missing name', $cre['name']));
+			// $this->assertMatchesRegularExpression('/([A-Za-z])///',$cre['name']);
+			$this->assertArrayHasKey('class', $cre, sprintf('%s missing class', $cre['name']));
+			
+			$this->assertArrayHasKey('epoch', $cre, sprintf('%s missing epoch', $cre['name']));
+			
+			$this->assertArrayHasKey('engine', $cre, sprintf('%s missing engine', $cre['name']));
+			
+			$this->assertArrayHasKey('server', $cre, sprintf('%s missing server', $cre['name']));
+			
+			if($cre['engine'] == 'metrc') {
+				$this->assertArrayHasKey(
+					'service-key', 
+					$cre, 
+					sprintf('%s missing service-key', $cre['name'])	
+				);
+			}
+		}
 	}
 }
