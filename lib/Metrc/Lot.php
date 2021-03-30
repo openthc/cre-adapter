5680ed60859ab8fd93e0e5172581c1a0744aef9d
openthc <code@openthc.com>
2021-03-30 00:59:38 +0000

    finish functions

--- lib/Metrc/Lot.php
+++ lib/Metrc/Lot.php
@@ -9,4 +9,26 @@ class Lot extends \OpenTHC\CRE\Metrc\Base
 {
 	protected $_path = '/packages/v1';
 
+	/**
+	 * Finish Array of Packages
+	 */
+	function finish($arg)
+	{
+		$url = $this->_client->_make_url('/packages/v1/finish');
+		$req = $this->_client->_curl_init($url);
+		$res = $this->_client->_curl_exec($req, $arg);
+		return $res;
+	}
+
+	/**
+	 * Finish Undo for Array of Lots
+	 */
+	function finish_undo($arg)
+	{
+		$url = $this->_client->_make_url('/packages/v1/unfinish');
+		$req = $this->_client->_curl_init($url);
+		$res = $this->_client->_curl_exec($req, $arg);
+		return $res;
+	}
+
 }
