b691e2529d0cc09f421ed00c79f0eb5604196bed
openthc <code@openthc.com>
2021-04-02 16:51:04 +0000

    patch path

--- lib/Metrc/B2C.php
+++ lib/Metrc/B2C.php
@@ -7,23 +7,24 @@ namespace OpenTHC\CRE\Metrc;
 
 class B2C extends \OpenTHC\CRE\Metrc\Base
 {
+	protected $_path = '/sales/v1/receipts';
+
 	function create($obj)
 	{
-		$url = $this->_client->_make_url('/sales/v1/receipts');
+		$url = $this->_client->_make_url($this->_path);
 		$req = $this->_client->_curl_init($url);
-		$arg = [ $arg ];
 		$res = $this->_client->_curl_exec($req, [ $obj ]);
 		return $res;
 	}
 
 	/**
-	 * Delete Strain
+	 * Delete B2C Sale
 	 * @param  [type] $id [description]
 	 * @return [type]     [description]
 	 */
 	function delete($id)
 	{
-		$url = sprintf('/sales/v1/receipts/%s', $id);
+		$url = sprintf('%s/%s', $this->_path, $id);
 		$url = $this->_client->_make_url($url);
 		$req = $this->_client->_curl_init($url);
 		curl_setopt($req, CURLOPT_CUSTOMREQUEST, 'DELETE');
@@ -32,14 +33,17 @@ class B2C extends \OpenTHC\CRE\Metrc\Base
 
 	}
 
-	// function strainList()
-	// @param $id ID of Strain to get, default 'active'
-	function search($q=null)
+	/**
+	 *
+	 */
+	function search($arg=null)
 	{
-		$url = sprintf('/sales/v1/receipts/%s', $q);
-		$url = rtrim($url, ' /');
-		$url = $this->_client->_make_url($url);
+		if (empty($arg)) {
+			$arg = 'active';
+		}
 
+		$url = sprintf('/%s/%s', $this->_path, $arg);
+		$url = $this->_client->_make_url($url);
 		$req = $this->_client->_curl_init($url);
 		$res = $this->_client->_curl_exec($req);
 
@@ -49,7 +53,7 @@ class B2C extends \OpenTHC\CRE\Metrc\Base
 
 	function update($arg)
 	{
-		$url = $this->_client->_make_url('/sales/v1/receipts');
+		$url = $this->_client->_make_url($this->_path);
 		$req = $this->_client->_curl_init($url);
 		curl_setopt($req, CURLOPT_CUSTOMREQUEST, 'PUT');
 		$res = $this->_client->_curl_exec($req, [ $arg ]);
