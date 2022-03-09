d08f28c64579da43281892568eae171bb9c698db
openthc <code@openthc.com>
2022-03-09 03:14:31 +0000

    fix case on product type map

--- lib/WCIA.php
+++ lib/WCIA.php
@@ -12,54 +12,65 @@ class WCIA extends \OpenTHC\CRE\Base
 {
 	const ENGINE = 'openthc';
 
-	function __construct() { /* NO */ }
+	/**
+	 * Could build a static map here?
+	 */
+	private static $product_type_map = [
+		'018NY6XC00PT25F95HPG583AJB' => 'EndProduct/Capsules',
+		'018NY6XC00PTBNDY5VJ8JQ6NKP' => 'EndProduct/Solid Edible',
+		'018NY6XC00PTAF3TFBB51C8HX6' => 'HarvestedMaterial/Flower Lot',
+		'018NY6XC00PTR9M5Z9S4T31C4R' => 'IntermediateProduct/CO2 Concentrate',
+	];
 
+	/**
+	 *
+	 */
+	function __construct() { /* NO */ }
 
 	/**
 	 * Remaps the WCIA Product Type
+	 * @return and OpenTHC Style Product Type ULID
 	 */
-	function _wcia_map_product_type_id($t0, $t1)
+	static function product_type_map_id($t0, $t1)
 	{
-		// $t0 = $x['meta']['_source']['inventory_category'];
-		// $t1 = $x['meta']['_source']['inventory_type'];
-		$pt = sprintf('%s/%s', $t0, $t1);
+		$pt = strtoupper(sprintf('%s/%s', $t0, $t1));
 		switch ($pt) {
-			case 'EndProduct/Capsules':                         return '018NY6XC00PT25F95HPG583AJB';
-			case 'EndProduct/Solid Edible':                     return '018NY6XC00PTBNDY5VJ8JQ6NKP';
-			case 'HarvestedMaterial/Flower Lot':                return '018NY6XC00PTAF3TFBB51C8HX6';
-			case 'IntermediateProduct/CO2 Concentrate':         return '018NY6XC00PTR9M5Z9S4T31C4R';
-			case 'IntermediateProduct/Hydrocarbon Concentrate': return '018NY6XC00PTCS5AZV189X1YRK';
-			case 'IntermediateProduct/Marijuana Mix':           return '018NY6XC00PT63ECNBAZH32YC3';
-			case 'IntermediateProduct/Non-Solvent Based Concentrate': return '018NY6XC00PTNPA4TPCYSKD5XN';
-			case 'IntermediateProduct/Concentrate for Inhalation': return '018NY6XC00PTNPA4TPCYSKD5XN';
+			case 'ENDPRODUCT/CAPSULES':                         return '018NY6XC00PT25F95HPG583AJB';
+			case 'ENDPRODUCT/SOLID EDIBLE':                     return '018NY6XC00PTBNDY5VJ8JQ6NKP';
+			case 'HARVESTEDMATERIAL/FLOWER LOT':                return '018NY6XC00PTAF3TFBB51C8HX6';
+			case 'INTERMEDIATEPRODUCT/CO2 CONCENTRATE':         return '018NY6XC00PTR9M5Z9S4T31C4R';
+			case 'INTERMEDIATEPRODUCT/HYDROCARBON CONCENTRATE': return '018NY6XC00PTCS5AZV189X1YRK';
+			case 'INTERMEDIATEPRODUCT/MARIJUANA MIX':           return '018NY6XC00PT63ECNBAZH32YC3';
+			case 'INTERMEDIATEPRODUCT/NON-SOLVENT BASED CONCENTRATE': return '018NY6XC00PTNPA4TPCYSKD5XN';
+			case 'INTERMEDIATEPRODUCT/CONCENTRATE FOR INHALATION': return '018NY6XC00PTNPA4TPCYSKD5XN';
 			default:
 				// Failsafe
 				switch ($t1) {
-					case 'Topical Ointment';         return '018NY6XC00PT0WQP2XV5KNP395';
-					case 'Capsules';                 return '018NY6XC00PT25F95HPG583AJB';
-					case 'Plant';                    return '018NY6XC00PT2BKFPCEFB9G1Z2';
-					case 'Plant';                    return '018NY6XC00PT3EZZ4GN6105M64';
-					case 'Ethanol Concentrate';      return '018NY6XC00PT684JJSXN8RAWBM';
-					case 'Liquid Edible';            return '018NY6XC00PT7N83PFNCX8ZFEF';
-					case 'Waste';                    return '018NY6XC00PT8AXVZGNZN3A0QT';
-					case 'Other Material Lot';       return '018NY6XC00PT8ZPGMPR8H2TAXH';
-					case 'Suppository';              return '018NY6XC00PTBJ3G5FDAJN60EX';
-					case 'Solid Edible';             return '018NY6XC00PTBNDY5VJ8JQ6NKP';
-					case 'Tincture';                 return '018NY6XC00PTD9Q4QPFBH0G9H2';
-					case 'Plant';                    return '018NY6XC00PTFY48D1136W0S0J';
-					case 'Other Material Unlotted';  return '018NY6XC00PTGBW49J6YD3WM84';
-					case 'Usable Marijuana';         return '018NY6XC00PTGMB39NHCZ8EDEZ';
-					case 'Marijuana Mix Infused';    return '018NY6XC00PTGRX4Q9SZBHDA5Z';
-					case 'Sample Jar';               return '018NY6XC00PTHE7GWB4QTG4JKZ';
-					case 'Food Grade Solvent Concentrate';    return '018NY6XC00PTHP9NMJ1RE6TA62';
-					case 'Transdermal';              return '018NY6XC00PTHPB8YG56S0MCAC';
-					case 'Marijuana Mix Packaged';   return '018NY6XC00PTKYYGMRSKV4XNH7';
-					case 'Non-Solvent Based Concentrate';
-					case 'Plant';                    return '018NY6XC00PTRPPDT8NJY2MWQW';
-					case 'Marijuana Mix Infused'; // Concentrate For Inhalation    return '018NY6XC00PTSF5NTC899SR0JF';
-					case 'Infused Cooking Medium';   return '018NY6XC00PTY5XPA4KJT6W3K4';
-					case 'Seed';                     return '018NY6XC00PTY9THKSEQ8NFS1J';
-					case 'Flower Unlotted';          return '018NY6XC00PTZZWCH7XVREHK6T';
+					case 'TOPICAL OINTMENT';         return '018NY6XC00PT0WQP2XV5KNP395';
+					case 'CAPSULES';                 return '018NY6XC00PT25F95HPG583AJB';
+					case 'PLANT';                    return '018NY6XC00PT2BKFPCEFB9G1Z2';
+					case 'PLANT';                    return '018NY6XC00PT3EZZ4GN6105M64';
+					case 'ETHANOL CONCENTRATE';      return '018NY6XC00PT684JJSXN8RAWBM';
+					case 'LIQUID EDIBLE';            return '018NY6XC00PT7N83PFNCX8ZFEF';
+					case 'WASTE';                    return '018NY6XC00PT8AXVZGNZN3A0QT';
+					case 'OTHER MATERIAL LOT';       return '018NY6XC00PT8ZPGMPR8H2TAXH';
+					case 'SUPPOSITORY';              return '018NY6XC00PTBJ3G5FDAJN60EX';
+					case 'SOLID EDIBLE';             return '018NY6XC00PTBNDY5VJ8JQ6NKP';
+					case 'TINCTURE';                 return '018NY6XC00PTD9Q4QPFBH0G9H2';
+					case 'PLANT';                    return '018NY6XC00PTFY48D1136W0S0J';
+					case 'OTHER MATERIAL UNLOTTED';  return '018NY6XC00PTGBW49J6YD3WM84';
+					case 'USABLE MARIJUANA';         return '018NY6XC00PTGMB39NHCZ8EDEZ';
+					case 'MARIJUANA MIX INFUSED';    return '018NY6XC00PTGRX4Q9SZBHDA5Z';
+					case 'SAMPLE JAR';               return '018NY6XC00PTHE7GWB4QTG4JKZ';
+					case 'FOOD GRADE SOLVENT CONCENTRATE';    return '018NY6XC00PTHP9NMJ1RE6TA62';
+					case 'TRANSDERMAL';              return '018NY6XC00PTHPB8YG56S0MCAC';
+					case 'MARIJUANA MIX PACKAGED';   return '018NY6XC00PTKYYGMRSKV4XNH7';
+					case 'NON-SOLVENT BASED CONCENTRATE';
+					case 'PLANT';                    return '018NY6XC00PTRPPDT8NJY2MWQW';
+					case 'MARIJUANA MIX INFUSED'; // Concentrate For Inhalation    return '018NY6XC00PTSF5NTC899SR0JF';
+					case 'INFUSED COOKING MEDIUM';   return '018NY6XC00PTY5XPA4KJT6W3K4';
+					case 'SEED';                     return '018NY6XC00PTY9THKSEQ8NFS1J';
+					case 'FLOWER UNLOTTED';          return '018NY6XC00PTZZWCH7XVREHK6T';
 				}
 				var_dump($x);
 				throw new \Exception(_(sprintf('Unexpected Product Type "%s"', $pt)));
@@ -67,6 +78,14 @@ class WCIA extends \OpenTHC\CRE\Base
 
 		return '018NY6XC00PR0DUCTTYPE00001'; // -orphan-
 
+	}
+
+	/**
+	 *
+	 */
+	static function product_type_map_name($id)
+	{
+		// switch ($id)
 	}
 
 }
