b6578f4b122d4004eca3c3fe0a17e6f79fb13a78
openthc <code@openthc.com>
2022-11-01 20:33:30 +0000

    copy in CCRS auth routine

--- lib/CCRS.php
+++ lib/CCRS.php
@@ -8,9 +8,80 @@
 
 namespace OpenTHC\CRE;
 
+use HeadlessChromium\BrowserFactory;
+use HeadlessChromium\Communication\Message;
+
 class CCRS extends \OpenTHC\CRE\Base
 {
-	const ENGINE = 'openthc';
+	const ENGINE = 'ccrs';
+
+	protected $cookie_list = [];
+
+	/**
+	 *
+	 */
+	function auth($username, $password)
+	{
+		// Save the cookies in bong.sqlite
+		// $bf = new BrowserFactory();
+		$bf = new BrowserFactory('/usr/bin/chromium');
+		// $bf = new BrowserFactory('node_modules/puppeteer/.local-chromium/linux-686378/chrome-linux/chrome');
+		$b = $bf->createBrowser([
+			// 'debugLogger' => 'php://stdout',
+			'noSandbox' => true,
+			// 'userDataDir' => sprintf('%s/chrome-profile', APP_ROOT)
+		]);
+
+		$page = $b->createPage();
+		$page->setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36');
+
+		// Main Page
+		$page->navigate('https://cannabisreporting.lcb.wa.gov/')->waitForNavigation();
+		$url0 = $page->getCurrentUrl();
+		echo "url0:{$url0}\n";
+
+		// Needs Authentication?
+		if (preg_match('/^https:\/\/secureaccess\.wa\.gov\/FIM2\/sps\/auth/', $url0)) {
+
+			// POST the Form?
+			$code = sprintf('document.querySelector("#username").value = "%s";', $username);
+			$page->evaluate($code);
+
+			// Password
+			$code = sprintf('document.querySelector("#password").value = "%s";', $password);
+			$page->evaluate($code);
+
+			// $page->screenshot()->saveToFile('ccrs0.png');
+			// $page->evaluate('document.querySelector("#submit-button-row input").click()')->waitForPageReload();
+
+			$page->mouse()->find('#submit-button-row input')->click();
+			$page->waitForReload();
+
+			$url1 = $page->getCurrentUrl();
+			echo "url1:{$url1}\n";
+			// $page->screenshot()->saveToFile('ccrs1.png');
+		} elseif (preg_match('/https:\/\/secureaccess\.wa\.gov\/FIM2\/sps\/sawidp\/saml20\/login/', $url0)) {
+			// OK ? Only see this one intermittently
+		} elseif (preg_match('/^https:\/\/cannabisreporting\.lcb\.wa\.gov\//', $url0)) {
+			// Authenticated
+		} else {
+			echo "No Match: $url0\n";
+		}
+
+		// Save Cookies
+		$cookie_out = [];
+		$cookie_jar = $page->getAllCookies();
+		foreach ($cookie_jar as $c) {
+			$a = (array)$c;
+			$a = array_shift($a);
+			$cookie_out[] = $a;
+		}
+
+		$this->cookie_list = $cookie_out;
+
+		return $cookie_out;
+
+	}
 
 	/**
 	 * Get Data Hash of a CSV File
