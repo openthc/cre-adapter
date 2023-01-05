652d70c7e4e3d706a8888c451d25548b3a32e336
openthc <code@openthc.com>
2023-01-05 01:32:08 +0000

    Add B2B Commit

--- lib/OpenTHC/B2B.php
+++ lib/OpenTHC/B2B.php
@@ -10,4 +10,17 @@ namespace OpenTHC\CRE\OpenTHC;
 class B2B extends Base
 {
 	protected $_path = '/b2b';
+
+	/**
+	 * Do the COMMIT thing
+	 */
+	function commit(string $oid)
+	{
+		$url = sprintf('%s/%s/commit', $this->_path, $oid);
+		$arg = [
+			'a' => 'commit',
+		];
+		$res = $this->_cre->post($url, $arg);
+		return $res;
+	}
 }
