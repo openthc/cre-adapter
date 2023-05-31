6c0d5494ec7624e47d70c00c60b894b90205ed25
openthc <code@openthc.com>
2023-05-31 22:39:54 +0000

    add delete to Base

--- lib/Metrc/Base.php
+++ lib/Metrc/Base.php
@@ -72,4 +72,18 @@ class Base extends \OpenTHC\CRE\Base
 		return $res;
 	}
 
+	/**
+	 * Delete a Section
+	 * @param [type] $id [description]
+	 * @return [type] [description]
+	 */
+	function delete($id)
+	{
+		$url = sprintf('%s/%s', $this->_path, $id);
+		$url = $this->_client->_make_url($url);
+		$req = $this->_client->_curl_init($url);
+		curl_setopt($req, CURLOPT_CUSTOMREQUEST, 'DELETE');
+		$res = $this->_client->_curl_exec($req);
+		return $res;
+	}
 }
