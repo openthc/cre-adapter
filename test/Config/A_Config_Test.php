2bac5db1b4109265f0a99b9162497febdc07effa
openthc <code@openthc.com>
2025-10-31 18:59:08 +0000

    improve config check

--- test/Config/A_Config_Test.php
+++ test/Config/A_Config_Test.php
@@ -41,38 +41,50 @@ class A_Config_Test extends \OpenTHC\CRE\Test\Base_Case
 		$this->assertCount(53, $cre_list);
 
 		foreach ($cre_list as $cre_code => $cre_data) {
-
-			$cre_code = str_replace('-', '/', $cre_code);
-
-			$this->assertEquals($cre_code, $cre_data['id']);
-			$this->assertEquals($cre_code, $cre_data['code']);
-
-			// 	$this->assertNotEmpty($cre);
-			$this->assertArrayHasKey('id', $cre_data, sprintf('CRE "%s" missing id', $cre_code));
-			$this->assertArrayHasKey('tz', $cre_data, sprintf('CRE "%s" missing TZ', $cre_code));
-			$this->assertArrayHasKey('code', $cre_data, sprintf('CRE "%s" missing code', $cre_code));
-			$this->assertArrayHasKey('name', $cre_data, sprintf('CRE "%s" missing name', $cre_code));
-			$this->assertArrayHasKey('class', $cre_data, sprintf('CRE "%s" missing class', $cre_code));
-			$this->assertArrayHasKey('epoch', $cre_data, sprintf('CRE "%s" missing epoch', $cre_code));
-			$this->assertArrayHasKey('engine', $cre_data, sprintf('CRE "%s" missing engine', $cre_code));
-
-			$this->assertContains($cre_data['engine'], [ 'biotrack', 'metrc', 'openthc' ]);
-			switch ($cre_data['engine']) {
-				case 'biotrack':
-					$this->assertArrayHasKey('server', $cre_data, sprintf('%s missing server', $cre_code));
-					break;
-				case 'metrc':
-					$this->assertArrayHasKey('server', $cre_data, sprintf('%s missing server', $cre_code));
-					$this->assertArrayHasKey('service-sk', $cre_data, sprintf('%s missing service-sk', $cre_code));
-					break;
-				case 'openthc':
-					$this->assertArrayHasKey('server', $cre_data, sprintf('%s missing server', $cre_code));
-					$this->assertArrayHasKey('service-sk', $cre_data, sprintf('%s missing service-sk', $cre_code));
-					break;
-				default:
-					$this->assertTrue(false, sprintf('CRE "%s" has an unknown engine "%s"', $cre_code, $cre_data['engine']));
-			}
-
+			$this->config_check($cre_code, $cre_data);
 		}
+	}
+
+	function test_config_app()
+	{
+		if (empty($_ENV['OPENTHC_TEST_APP_ROOT'])) {
+			$this->markTestSkipped('Skipped, No App_TEST');
+		}
+
+		$cre_list = \OpenTHC\CRE::getEngineList($_ENV['OPENTHC_TEST_APP_ROOT']);
+		foreach ($cre_list as $cre_code => $cre_data) {
+			$this->config_check($cre_code, $cre_data);
+		}
+
+	}
+
+	protected function config_check($cre_code, $cre_data)
+	{
+		$this->assertArrayHasKey('id', $cre_data, sprintf('CRE "%s" missing id', $cre_code));
+		$this->assertArrayHasKey('tz', $cre_data, sprintf('CRE "%s" missing TZ', $cre_code));
+		$this->assertArrayHasKey('code', $cre_data, sprintf('CRE "%s" missing code', $cre_code));
+		$this->assertArrayHasKey('live', $cre_data, sprintf('CRE "%s" missing attribute "live"', $cre_code));
+		$this->assertArrayHasKey('name', $cre_data, sprintf('CRE "%s" missing name', $cre_code));
+		$this->assertArrayHasKey('class', $cre_data, sprintf('CRE "%s" missing class', $cre_code));
+		$this->assertArrayHasKey('epoch', $cre_data, sprintf('CRE "%s" missing epoch', $cre_code));
+		$this->assertArrayHasKey('engine', $cre_data, sprintf('CRE "%s" missing engine', $cre_code));
+
+		$this->assertContains($cre_data['engine'], [ 'biotrack', 'metrc', 'openthc' ]);
+		switch ($cre_data['engine']) {
+			case 'biotrack':
+				$this->assertArrayHasKey('server', $cre_data, sprintf('%s missing server', $cre_code));
+				break;
+			case 'metrc':
+				$this->assertArrayHasKey('server', $cre_data, sprintf('%s missing server', $cre_code));
+				$this->assertArrayHasKey('service-sk', $cre_data, sprintf('%s missing service-sk', $cre_code));
+				break;
+			case 'openthc':
+				$this->assertArrayHasKey('server', $cre_data, sprintf('%s missing server', $cre_code));
+				$this->assertArrayHasKey('service-sk', $cre_data, sprintf('%s missing service-sk', $cre_code));
+				break;
+			default:
+				$this->assertTrue(false, sprintf('CRE "%s" has an unknown engine "%s"', $cre_code, $cre_data['engine']));
+		}
+
 	}
 }
